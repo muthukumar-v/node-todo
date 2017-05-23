0d8d39862c8b921d1533fae0cad6f53e566eeaf7
unknown <v.muthukumar.in@hotmail.com>
2017-05-23 10:49:07 +0530

    add delete script

--- todo.js
+++ todo.js
@@ -10,23 +10,40 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db)=>{
     }
     console.log('Connected To DB Server...');
 
-//     db.collection('User').insertOne({
-//         name: 'Raju',
-//         age: 37,
-//         location: 'Bangalore'
-//     },(err, result)=>{
-//         if(err){
-//             console.log('Erro insert '+ err);
-//         }
-//         console.log(JSON.stringify(result.ops, undefined, 2));
-//     });
+    // //Insert Data
+    // db.collection('User').insertOne({
+    //     name: 'Raju',
+    //     age: 37,
+    //     location: 'Bangalore'
+    // },(err, result)=>{
+    //     if(err){
+    //         console.log('Erro insert '+ err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
 
+    // //View Data
+    // db.collection('User').find({location: 'Chennai'}).toArray().then((docs)=>{
+    //     console.log('User');
+    //     console.log(JSON.stringify(docs,undefined,2));
+    // }, (err) => {
+    //     console.log(err);
+    // });
 
-    db.collection('User').find({location: 'Chennai'}).toArray().then((docs)=>{
-        console.log('User');
-        console.log(JSON.stringify(docs,undefined,2));
-    }, (err) => {
-        console.log(err);
+    //Delete Many Data
+    // db.collection('User').deleteMany({name: 'Test'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //Delete One
+    // db.collection('User').deleteOne({name:'Muthukumar'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    //Delete FindOneandDelete
+    db.collection('User').findOneAndDelete({_id: new ObjectID("5923c3bdb2b73b7d9833d0e2")}).then((result)=>{
+        console.log(result);
     });
-   //db.close();
+
+   db.close();
 });
